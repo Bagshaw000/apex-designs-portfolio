4d332170c04441c78244d632d1a514f637fa44dc
Omieibi Bagshaw <62633996+Bagshaw000@users.noreply.github.com>
2026-03-26 03:03:29 +1300

    Completed 80% of the home page

--- synergis/component/header.tsx
+++ synergis/component/header.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import Image from "next/image";
-import whiteLogo from "../public/Synergis_Full_Black_Transparent.svg";
+import blackLogo from "../public/Synergis_Full_Black_Transparent.svg";
+import whiteLogo from "../public/Synergis_Full_White_Transparent.svg";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
+  const [menu,setMenu] = useState(false)
   const reachedBottom = useIsAtBottom(100);
   useEffect(() => {
     if (reachedBottom) {
@@ -13,21 +16,30 @@ export default function Header() {
       // Load more items
     }
   }, [reachedBottom]);
+
+  const handleShowMenu = () =>{
+    setMenu(!menu)
+  }
   return (
-    <div className="w-[90vw] z-100 max-w-125    rounded-2xl font-bitcount fixed top-[2vh]  left-1/2 transform -translate-x-1/2  text-black bg-[#2e2e2e0c]">
-      <div className="flex justify-between items-center px-5 h-[7vh] hover:h-[9vh] delay-100 duration-200 md:text-xs">
+    <div onClick={handleShowMenu} className="w-[90vw] z-100 max-w-125    rounded-2xl font-bitcount fixed top-[2vh]  left-1/2 transform -translate-x-1/2  text-black  " style={reachedBottom ? {backgroundColor:"blue", color:"white"}:  {backgroundColor:"#ffffff33"}}>
+      <div className="flex justify-between items-center px-5 h-[7vh] hover:h-[9vh] delay-100 duration-200 md:text-xs" >
         <Link href={""}>
-          <h1 className=" decoration-black" style={{ color: "black" }}>
+          <h1 className="">
             WORK
           </h1>
         </Link>
 
         <div className="h-10 w-48 overflow-hidden ">
-          <Image
+          {!reachedBottom?<Image
+            className="h-full w-full md:scale-70 object-cover object-center"
+            src={blackLogo}
+            alt=""
+          />: <Image
             className="h-full w-full md:scale-70 object-cover object-center"
             src={whiteLogo}
             alt=""
-          />
+          />}
+          
         </div>
 
         <Link href={""}>
@@ -35,26 +47,59 @@ export default function Header() {
         </Link>
       </div>
 
-      <div className="mt-10">
-        <table className="table-fixed w-full font-inter text-center text-sm">
-          <tbody className="">
-            <tr className="">
-              <td className="border p-3 border-l-0"> <Link href={""}>Expertise & Capabilities</Link></td>
-              <td className="border p-3 border-r-0"> <Link href={""}>Process & Pricing</Link></td>
-            </tr>
-            <tr>
-              <td className="border p-3 border-l-0"><Link href={""}>Outcomes</Link></td>
-              <td className="border p-3 border-r-0"> <Link href={""}>Teams</Link></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      {/* Finish the  */}
-      <div className="p-5">
-        <h1 className="font-bitcount text-sm">
-          General Enquires
-        </h1>
-      </div>
+      {!reachedBottom ? ( menu ?  <>
+          <div className="mt-10 ">
+            <table className="table-fixed w-full font-inter text-center text-sm">
+              <tbody className="">
+                <tr className="">
+                  <td className="border p-3 border-l-0">
+                    {" "}
+                    <Link href={""}>Expertise & Capabilities</Link>
+                  </td>
+                  <td className="border p-3 border-r-0">
+                    {" "}
+                    <Link href={""}>Process & Pricing</Link>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border p-3 border-l-0">
+                    <Link href={""}>Outcomes</Link>
+                  </td>
+                  <td className="border p-3 border-r-0">
+                    {" "}
+                    <Link href={""}>Teams</Link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          {/* Finish the  */}
+          <div className="p-5 mt-5">
+            <h1 className="font-bitcount text-sm">General Enquires</h1>
+            <div className="group flex flex-row items-center h-fit">
+              <div className="h-2 w-2 bg-blue-900 hidden group-hover:block duration-200 delay-200  ease-in-out"></div>
+              <span className="font-semibold  hover:translate-x-3 duration-200 ease-in-out delay-200">
+                info@synergisdesignsolutions.com
+              </span>
+            </div>
+
+            <div className="group flex flex-row items-center h-fit">
+              <div className="h-2 w-2 bg-blue-900 hidden group-hover:block duration-200 delay-100 ease-in-out"></div>
+              <Link href={""} className="hover:translate-x-3 font-semibold ">
+                Plan a call
+              </Link>
+            </div>
+          </div>
+        </>: <></>
+       
+      ) : (
+        < div className="p-5 text-center mt-15 w-fit md:w-[70%] mx-auto">
+          <h1 className="text-xl font-bold mb-5"> Industry insight & agency updates</h1>
+          <input type="text" name="" id="" className="bg-[#b8b8b833] h-10 w-full text-sm p-3 mb-3 rounded-lg text-center border-0 border-transparent active:border-0 focus:border-0" placeholder="brandon@synergisdesign.com"/>
+          {/* <input type="button" value="" /> */}
+          <Button className="bg-white text-black w-full h-10">Submit</Button>
+        </div>
+      )}
     </div>
   );
 }
